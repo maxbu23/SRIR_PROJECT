106ad28eefea6d4f6814f2010b30f5f5b26ea0ea
Maksymilian Buczek <0buczek@taurus.fis.agh.edu.pl>
2024-05-03 00:53:04 +0200

    Init commit

---
+++ node.cpp
@@ -0,0 +1,46 @@
+#include "node.h"
+#include <fstream>
+#include <string>
+
+// std::array<std::array<int, N>, N> Node::finalBoard = {{{{0,1,2}}, {{3,4,5}}, {{6,7,8}}}};
+
+int isSafe(int x, int y)
+{
+    return (x >= 0 && x < N && y >= 0 && y < N);
+}
+
+std::array<std::array<int, N>, N> getInputArray(const std::string& filename)
+{
+    std::ifstream file(filename, std::ios::in);
+    std::string line;
+    std::array<std::array<int, 3>, 3> initial{};
+
+    int i = 0;
+    while (std::getline(file, line))
+    {
+        int j = 0;
+        for (char c : line)
+        {
+            if (c != ' ')
+            {
+                initial[i][j++] = c - '0';
+            }
+        }
+        i++;
+    }
+
+    return initial;
+}
+
+std::array<std::array<int, N>, N> Node::finalBoard = getInputArray("final.txt");
+
+
+std::pair<int, int> getBlankPosition(const std::array<std::array<int, N>, N>& board)
+{
+    for (size_t i=0; i<board.size(); i++)
+        for (size_t j=0; j<board[i].size(); j++)
+            if (board[i][j] == 0)
+                return std::make_pair(i, j);
+                
+    return std::make_pair(-1, -1);
+}
